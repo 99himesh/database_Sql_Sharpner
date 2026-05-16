98bc2b09df37488c1e61939e149a692a8b38fe88
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-05-16 11:42:13 +0530

    create table booking app

--- index.js
+++ index.js
@@ -17,30 +17,46 @@ connection.connect((err)=>{
     return;
   }
   console.log("Connection has been created");
-  
-  const createQuery=`CREATE TABLE students(
-    id int primary key,
-    name varchar(20),
-    email varchar(20)
-  )`
-  console.log("Table is created");
-  connection.execute(createQuery,(err)=>{
+  const createQuery=[
+             `CREATE TABLE Users(
+              id int primary key,
+              name varchar(20),
+              email varchar(20)
+            )`,
+            `CREATE TABLE Busses(
+              id int primary key,
+              busNumber int,
+              totalSeats int,
+              availableSeats int
+            )`,
+            `CREATE TABLE Bookings(
+              id int primary key,
+              seatNumber int
+            )`,
+            `CREATE TABLE Payments(
+              id int primary key,
+              amountPaid int,
+              paymentStatus int
+            )`
+];
+
+
+createQuery.map((query)=>{
+  return connection.execute(query,(err)=>{
     if(err){
-        console.log(err);
         connection.end();
-        return;
-        
+        return;   
     }
     console.log("table is created");
     
   })
 })
+  
+})
 
 app.get("/",(req,res)=>{
     res.send("Hello world")
 });
-
-
 
 app.listen(3000,()=>{
     console.log("Server is runnning");
